7ce7067b5e93ad2a0552a9efb7a605792b118e21
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-09-18 16:22:20 +0300

    added serhcing

--- src/modules/Post/post.controller.ts
+++ src/modules/Post/post.controller.ts
@@ -13,7 +13,8 @@ const getAllPosts = async (req: Request, res: Response) => {
   try {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
-    const result = await PostService.getAllPosts({ page, limit });
+    const search = req.params.search || "";
+    const result = await PostService.getAllPosts({ page, limit, search });
     res.json(result);
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch posts", details: err });
